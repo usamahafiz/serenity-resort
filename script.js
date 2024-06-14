5e369fff1a74f02fc765ea88044ed8d5c976d90f
Hafiz usama <160656348+usamahafiz@users.noreply.github.com>
2024-06-15 01:28:57 +0500

    srenety-oasis resort website
    this website based on a resorts beauty and their facilities

---
+++ script.js
@@ -0,0 +1,18 @@
+function submit() {
+    var email = document.getElementById("email").value;
+    if (!email) {
+        Toastify({
+            text: "Please write the required credentials",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+          }).showToast();
+    }
+}
